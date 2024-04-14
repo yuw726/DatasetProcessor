9c9d6326ac0c99072358d831d7bbbbc0aa139c5b
Volodymyr Yurchenko <yuw726@gmail.com>
2024-04-14 22:15:24 +0200

    Add unit tests for data processor

--- src/test/java/org/example/DatasetProcessorTest.java
+++ src/test/java/org/example/DatasetProcessorTest.java
@@ -40,4 +40,46 @@ class DatasetProcessorTest {
         // The number of lines in the test file is 1000000 + header
         assertEquals(1000001, lineCount);
     }
+
+    @Test
+    void givenTestFile_whenCheckRandomLine_thenSuccess() {
+        Path inputPath = Paths.get("input.csv");
+        assertTrue(Files.exists(inputPath));
+        Path outputPath = Paths.get("output.csv");
+        assertTrue(Files.exists(outputPath));
+
+        int randomLine = (int) (Math.random() * 1000000) + 1;
+
+        int value1 = 0;
+        double value2 = 0.0;
+
+        try (BufferedReader br = Files.newBufferedReader(inputPath)) {
+            for (int i = 0; i < randomLine; i++) {
+                br.readLine();
+            }
+            String line = br.readLine();
+            assertNotNull(line);
+
+            String[] fields = line.split(",");
+            value1 = Integer.parseInt(fields[0]);
+            value2 = Double.parseDouble(fields[1]);
+        } catch (Exception e) {
+            fail(e);
+        }
+
+        assertDoesNotThrow(() -> processor.process("input.csv", "output.csv"));
+
+        try (BufferedReader br = Files.newBufferedReader(outputPath)) {
+            for (int i = 0; i < randomLine; i++) {
+                br.readLine();
+            }
+            String line = br.readLine();
+            assertNotNull(line);
+            String[] fields = line.split(",");
+            assertEquals(String.format("%.8f", value1 + value2), fields[0]);
+            assertEquals(String.format("%.8f", value1 * value2), fields[1]);
+        } catch (Exception e) {
+            fail(e);
+        }
+    }
 }
